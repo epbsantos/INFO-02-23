75219496dc934435826cfaedc0f0b2c55417344d
Cesar Fuentes <cesar@fuentes.net.br>
2023-10-24 18:14:42 -0300

    termino pesquisa api imet

--- api_tempo/js/scripts.js
+++ api_tempo/js/scripts.js
@@ -1,56 +1,3 @@
-//variaveis de pesquisa de cep
-var pesquisaCEP, pesquisaRua, pesquisaBairro, pesquisaCidadeUf, pesquisaLatitude, pesquisaLongitude;
-
-
-// mascara para CEP no botão de pesquisa por cep
-const element = document.getElementById('CEP');
-const maskOptions = {
-    mask: '00000-000'
-};
-const mask = IMask(element, maskOptions);
-
-// captura click em pesquisar cep
-var botaoPesquisar = document.getElementById("pesquisa_cep");
-botaoPesquisar.addEventListener("click", function () {
-    var campoCEP = document.getElementById("CEP");
-    var valorCEP = campoCEP.value;
-
-    fetch("https://brasilapi.com.br/api/cep/v2/" + valorCEP )
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            temp = data;
-            pesquisaCEP = data.cep;
-            pesquisaRua = data.street;
-            pesquisaBairro = data.neighborhood;
-            pesquisaCidadeUf = data.city + "/" + data.state;
-            pesquisaLongitude = data.location.coordinates.longitude;
-            pesquisaLatitude = data.location.coordinates.latitude;
-
-            document.getElementById("respCep").innerHTML = pesquisaCEP;
-            document.getElementById("respRua").innerHTML = pesquisaRua;
-            document.getElementById("respBairro").innerHTML = pesquisaBairro;
-            document.getElementById("respCidadeUf").innerHTML = pesquisaCidadeUf;
-            document.getElementById("respLatitude").innerHTML = pesquisaLatitude;
-            document.getElementById("respLongitude").innerHTML = pesquisaLongitude;
-        })
-        .catch(function (error) {
-            console.error("Ocorreu um erro:", error);
-        });
-});
-
-fetch("../resources/estacoes_automaticas.json")
-    .then((response) => response.json())
-    .then((data) => {
-        estacoes = data;
-    })
-    .catch((error) => {
-        console.error("Erro ao carregar o arquivo JSON:", error);
-    });
-
-
-
 function calcularDistancia(lat1, lon1, lat2, lon2) {
     const raioTerra = 6371;
 
@@ -71,14 +18,14 @@ function calcularDistancia(lat1, lon1, lat2, lon2) {
     return distancia;
 }
 
-function encontrarEstacaoMaisProxima() {
+function encontrarEstacaoMaisProxima(latitude, longitude) {
     let estacaoMaisProxima = null;
     let distanciaMinima = Infinity;
 
     for (const estacao of estacoes) {
         const distancia = calcularDistancia(
-            pontoDeReferencia.latitude,
-            pontoDeReferencia.longitude,
+            latitude,
+            longitude,
             estacao.VL_LATITUDE,
             estacao.VL_LONGITUDE
         );
@@ -88,15 +35,114 @@ function encontrarEstacaoMaisProxima() {
             estacaoMaisProxima = estacao;
         }
     }
-    return estacaoMaisProxima;
+
+    return {
+        'estacao': estacaoMaisProxima,
+        'distancia': distanciaMinima
+    };
 }
 
-const estacaoProxima = encontrarEstacaoMaisProxima();
+// pega estações automaticas;
+fetch("../resources/estacoes_automaticas.json")
+    .then((response) => response.json())
+    .then((data) => {
+        estacoes = data;
+    })
+    .catch((error) => {
+        console.error("Erro ao carregar o arquivo JSON:", error);
+    });
 
-if (estacaoProxima) {
-    console.log(
-        "Estação mais próxima: " + estacaoProxima.DC_NOME + ", Distância: " + distanciaMinima + " km"
-    );
-} else {
-    console.log("Nenhuma estação encontrada na lista.");
-}
+//variaveis de pesquisa de cep
+var pesquisaCEP, pesquisaRua, pesquisaBairro, pesquisaCidadeUf, pesquisaLatitude, pesquisaLongitude, estacaoProxima, leituras;
+
+
+// mascara para CEP no botão de pesquisa por cep
+const element = document.getElementById('CEP');
+const maskOptions = {
+    mask: '00000-000'
+};
+const mask = IMask(element, maskOptions);
+
+// mascara para data no botão de pesquisa por data
+const element2 = document.getElementById('dateStart');
+const element3 = document.getElementById('dateEnd');
+const maskOptions2 = {
+    mask: '00/00/0000'
+};
+const mask2 = IMask(element2, maskOptions2);
+const mask3 = IMask(element3, maskOptions2);
+
+// captura click em pesquisar cep
+var botaoPesquisar = document.getElementById("pesquisa_cep");
+botaoPesquisar.addEventListener("click", function () {
+    var campoCEP = document.getElementById("CEP");
+    var valorCEP = campoCEP.value;
+
+    fetch("https://brasilapi.com.br/api/cep/v2/" + valorCEP )
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            pesquisaCEP = data.cep;
+            pesquisaRua = data.street;
+            pesquisaBairro = data.neighborhood;
+            pesquisaCidadeUf = data.city + "/" + data.state;
+            pesquisaLongitude = data.location.coordinates.longitude;
+            pesquisaLatitude = data.location.coordinates.latitude;
+
+            document.getElementById("respCep").innerHTML = pesquisaCEP;
+            document.getElementById("respRua").innerHTML = pesquisaRua;
+            document.getElementById("respBairro").innerHTML = pesquisaBairro;
+            document.getElementById("respCidadeUf").innerHTML = pesquisaCidadeUf;
+            document.getElementById("respLatitude").innerHTML = pesquisaLatitude;
+            document.getElementById("respLongitude").innerHTML = pesquisaLongitude;
+
+            estacaoProxima = encontrarEstacaoMaisProxima(pesquisaLatitude, pesquisaLongitude);
+
+            if (estacaoProxima) {
+                document.getElementById("infoEstacao").innerHTML = "<strong>Estação: </strong>" + 
+                                    estacaoProxima.estacao.DC_NOME + 
+                                    " (" + Math.floor(estacaoProxima.distancia) + "km)";
+
+                const dateStart = document.getElementById('dateStart');
+                const dateEnd = document.getElementById('dateEnd');
+                const searchButton = document.getElementById('searchButton');
+                
+                dateStart.disabled = false;
+                dateEnd.disabled = false;
+                searchButton.disabled = false;
+            } else {
+                console.log("Nenhuma estação encontrada na lista.");
+            }
+        })
+        .catch(function (error) {
+            console.error("Ocorreu um erro:", error);
+        });
+});
+
+var searchButton = document.getElementById("searchButton");
+searchButton.addEventListener("click", function () {
+    dataInicio = document.getElementById('dateStart').value;
+    dataFim = document.getElementById('dateEnd').value;
+
+    dataInicioFormatada = dataInicio.split('/').reverse().join('-');
+    dataFimFormatada = dataFim.split('/').reverse().join('-');
+
+    urlApi = "https://apitempo.inmet.gov.br/token/estacao/diaria/" + 
+                dataInicioFormatada + "/" + 
+                dataFimFormatada + "/" + 
+                estacaoProxima.estacao.CD_ESTACAO + "/" +
+                "eUlqdXRLU29rV3lybkhIYmJRYlJvSTFlY3gxdjhpU00=yIjutKSokWyrnHHbbQbRoI1ecx1v8iSM";
+
+    fetch(urlApi)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            leituras = data;
+            console.log(leituras, estacaoProxima);
+        })
+        .catch(function (error) {
+            console.error("Ocorreu um erro:", error);
+        });
+});
