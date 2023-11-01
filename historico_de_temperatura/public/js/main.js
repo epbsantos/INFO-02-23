c8e20db650907b99b3bcf8459b3e7bb7e26bce32
dumaciel <eduardo.maciel@convicti.com.br>
2023-11-01 20:55:24 -0300

    adds chart to present data

--- historico_de_temperatura/public/js/main.js
+++ historico_de_temperatura/public/js/main.js
@@ -101,3 +101,24 @@ $(document).ready(function () {
         }
     });
 });
+
+const lineChart = document.getElementById('lineChart');
+const data = {
+    labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+        'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+    datasets: [{
+        label: 'My First Dataset',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1
+    }]
+};
+const config = {
+    type: 'line',
+    data: data,
+};
+const stackedLine = new Chart(lineChart, {
+    type: 'line',
+    data: data,
+});
