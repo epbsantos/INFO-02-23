f459a4c123c2cdccf066fe2898d7ca4402a26757
Cesarwrze <137361385+Cesarwrze@users.noreply.github.com>
2023-11-05 18:04:43 -0300

    Add files via upload

--- enchentes/js/script.js
+++ enchentes/js/script.js
@@ -1,22 +1,6 @@
 document.querySelector('#fullview').setAttribute('tabindex', 0);
 document.querySelector('#fullview').focus();
 
-document.addEventListener('DOMContentLoaded', function() {
-    var fullview = new Fullview('#fullview', {
-        direction: 'horizontal',
-        loop: true, // Para permitir a rolagem contínua
-    });
-
-    var downButton = document.getElementById('down');
-        downButton.addEventListener('click', function() {
-            fullview.next();
-        });
-            
-    document.addEventListener('keydown', function(event) {
-        if (event.key === 'ArrowRight') {
-            fullview.next(); // Navegar para a próxima seção quando a seta direita for pressionada
-        } else if (event.key === 'ArrowLeft') {
-            fullview.prev(); // Navegar para a seção anterior quando a seta esquerda for pressionada
-        }
-    });
-});
+$("#fullview").fullView({
+	navbar:  "#navbar",
+})
