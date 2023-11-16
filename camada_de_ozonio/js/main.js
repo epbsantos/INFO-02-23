e0eb472eeb969e5803f5e4d9ff211201a9fcbba3
ArthurES <126832954+ArthurE-SC@users.noreply.github.com>
2023-11-15 23:08:25 -0300

    Redirect to an URL on click functions added

--- camada_de_ozonio/js/main.js
+++ camada_de_ozonio/js/main.js
@@ -17,3 +17,19 @@ var swiper2 = new Swiper(".bg-slider", {
     swiper: swiper,
   },
 });
+
+document.getElementById("myButton1").onclick = function () {
+  location.href = "https://www.wwf.org.br/natureza_brasileira/questoes_ambientais/camada_ozonio/";
+};
+
+document.getElementById("myButton2").onclick = function () {
+  location.href = "https://ozone.unep.org/ozone-and-you#:~:text=When%20an%20ozone%20molecule%20absorbs,radiation%20from%20reaching%20Earth%27s%20surface.";
+};
+
+document.getElementById("myButton3").onclick = function () {
+  location.href = "https://brasilescola.uol.com.br/geografia/camada-de-ozonio.htm#Destruição+da+camada+de+ozônio";
+};
+
+document.getElementById("myButton4").onclick = function () {
+  location.href = "https://www.bbc.com/portuguese/geral-59053884#:~:text=Os%20pesquisadores%20estimam%20que%20até,estar%20totalmente%20recomposta%20em%202060.";
+};
